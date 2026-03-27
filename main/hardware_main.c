4609b7e6a3ae996b1eb56d6f938cfef8e522d4ca
Andrew Hoult <mrrobottrax@gmail.com>
2026-03-27 16:52:29 -0400

    Find i2c devices

---
+++ main/hardware_main.c
@@ -0,0 +1,48 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#include "driver/i2c_master.h"
+
+void app_main(void)
+{
+    i2c_master_bus_config_t i2c_mst_config_1 = {
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .i2c_port = 0,
+        .scl_io_num = 22,
+        .sda_io_num = 21,
+        .glitch_ignore_cnt = 7,
+        .flags.enable_internal_pullup = true,
+    };
+    i2c_master_bus_handle_t bus_handle;
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config_1, &bus_handle));
+
+    for (int i = 0; i <= 0x7F; ++i)
+    {
+        if (i2c_master_probe(bus_handle, i, -1) == ESP_OK)
+        {
+            printf("Found I2C %i\n", i);
+        }
+    }
+
+    ESP_ERROR_CHECK(i2c_del_master_bus(bus_handle));
+
+    for (int i = 10; i >= 0; i--)
+    {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
+}
